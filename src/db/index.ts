5640f5285b37971f13dd870a7313239148efc7e2
Ahmad Syaifuddin <fiays@users.noreply.github.com>
2026-09-18 19:32:27 +0700

    fix ssl connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,11 @@ import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as schema from './schema';
 
-const connection = await mysql.createConnection(process.env.DATABASE_URL!);
+const connection = await mysql.createConnection({
+  uri: process.env.DATABASE_URL!.replace('?ssl-mode=REQUIRED', ''),
+  ssl: {
+    rejectUnauthorized: true,
+  },
+});
 
 export const db = drizzle(connection, { schema, mode: 'default' });
